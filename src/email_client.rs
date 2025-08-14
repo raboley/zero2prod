7c126892ca7641539643f3f219449a0ee92b6af9
Russell Boley <raboley@gmail.com>
2025-08-14 15:33:24 -0700

    7.2.5.1 Refactoring: Removing allocations
    * Updated all the owned strings to refence to slice of strings in the
    EmailBody struct. * Serde JSON does the heavy lifting to get the string
    slices to our thing correctly. * We just had to specify the lifetime for
    all our properties to be the same as our object using <'a> and &'a str,

--- src/email_client.rs
+++ src/email_client.rs
@@ -42,11 +42,11 @@ impl EmailClient {
         text_content: &str,
     ) -> Result<(), reqwest::Error> {
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject: subject,
+            html_body: html_content,
+            text_body: text_content,
         };
         self.http_client
             .post(self.email_url.clone())
@@ -72,12 +72,12 @@ impl EmailClient {
 
 #[derive(serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 #[cfg(test)]
